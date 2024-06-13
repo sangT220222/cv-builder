8e49a3d0f0b0d1dfaea3f221720fc8b04def257d
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-13 17:08:59 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
+import GeneralInfo from './generalInfo'
 
-import './App.css'
+import '../styles/App.css'
 
 function App() {
+  const[generalInfo, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  })
 
+  const handleGeneralInfo = (info) => {
+    setGeneralInfo(info);
+  }
+
+  return(
+    <div className = 'app'>
+      <h1>CV Builder</h1>
+      <GeneralInfo generalInfo = {generalInfo} onGeneralInfoChange = {handleGeneralInfo}/>
+    </div>
+  )
 }
 
 export default App
