c1d93cff8de794324899d68ab832d754593c4ec4
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-13 17:09:09 +0100

    Update generalInfo.jsx

---
+++ src/components/generalInfo.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+
+function GeneralInfo({generalInfo, onGeneralInfoChange}){
+  const [isEditing, setIsEditing] = useState(true);
+  const [info, setInfo] = useState(generalInfo)
+
+  const handleChange = (e) => {
+    const {name,value} = e.target; //name corresponds the name attribute of the input field (name, email, phone)
+    //value is the current value entered by user in the input field
+    setInfo((prevInfo) => ({...prevInfo, [name]: value}));
+    //prevInfo is the previous state of setInfo,
+    //...prevInfo creates a copy of the prevInfo object
+    //[name] evaluates the value of the name variable can be either -> name email or phone
+    //value is current value in the input field
+  }
+
+  const handleSubmit = () =>{
+    onGeneralInfoChange(info);
+    setIsEditing(false);
+  }
+
+  const handleEdit = () =>{
+    setIsEditing(true);
+  }
+
+  return(
+    <div className="general-info">
+      <h2>General Information</h2>
+      {isEditing ? (
+        <div>
+          <label>
+            Name:
+            <input type = "text" name = "name" value = {info.name} onChange = {handleChange}></input>
+          </label>
+          <label>
+            Email:
+            <input type = "text" name = "email" value = {info.name} onChange = {handleChange}></input>
+          </label>
+          <label>
+            Phone:
+            <input type = "text" name = "phone" value = {info.name} onChange = {handleChange}></input>
+          </label>
+          <button onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      ) :
+        <div>
+          <p>Name : {info.name};</p>
+          <p>Email : {info.email}</p>
+          <p>Phone number : {info.phone}</p>
+        </div>
+      }
+    </div>
+    
+  )
+} 
+
+export default GeneralInfo;
