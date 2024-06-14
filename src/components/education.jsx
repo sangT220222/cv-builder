7db25ea86f23ce543ae739ae1a49e7c094e3caec
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-14 12:57:07 +0100

    Update education.jsx

---
+++ src/components/education.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+function Education({experience, onExperienceChange}){
+  const [isEditing, setIsEditing] = useState(true);
+  const [info, setInfo] = useState(experience);
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setInfo((prevInfo) => ({...prevInfo, [name] : value}));
+  }
+
+  const handleSubmit = () => {
+    onExperienceChange(info);
+    setIsEditing(false);
+  }
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  }
+
+  return(
+    <div className="experience">
+      <h2>Experience</h2>
+      {isEditing ? (
+        <div>
+          <label>
+            Title
+            <input type = "text" name = "title" value = {info.title} onChange = {handleChange}></input>
+          </label>
+          <label>
+            School Name
+            <input type = "text" name = "schoolName" value = {info.schoolName} onChange = {handleChange}></input>
+          </label>
+          <label>
+            Study Dates
+            <input type = "text" name = "studyDate" value = {info.studyDate} onChange = {handleChange}></input>
+          </label>
+          <button onClick = {handleSubmit}> Submit </button>
+        </div>
+      ):(
+        <div>
+          <p>Title: {info.title}</p>
+          <p>School Name: {info.schoolName}</p>
+          <p>Study dates: {info.studyDate}</p>
+          <button onClick = {handleEdit}> Edit </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Education;
