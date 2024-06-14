92a3310d95985531dca1366847c010f4654d44b4
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-14 15:48:41 +0100

    Update experience.jsx

---
+++ src/components/experience.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+function Experience({experience, onExperienceChange}){
+  const[isEditing, setIsEditing] = useState(true);
+  const[info, setInfo] = useState(experience);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setInfo((prevInfo) => ({...prevInfo , [name] : value}));
+  }
+
+  const handleSubmit = () =>{
+    setIsEditing(false);
+  } 
+
+  const handleEdit = () =>{
+    onExperienceChange(info);
+    setIsEditing(true);
+  }
+
+  return(
+    <div className="experience">
+      <h2>Experience</h2>
+      {isEditing ? (
+        <div>
+          <label>
+            Job Title
+            <input type = "text" name = "jobTitle" value = {info.jobTitle} onChange={handleChange}></input>
+          </label>
+          <label>
+            Date of Work
+            <input type = "text" name = "dateOfWork" value = {info.dateOfWork} onChange={handleChange}></input>
+          </label>
+          <label>
+            Responsibilities
+            <input type = "text" name = "responsibilities" value = {info.responsibilities} onChange={handleChange}></input>
+          </label>
+          <button onClick = {handleSubmit}>Submit</button>
+        </div>
+      ):(
+        <div>
+          <p>Job Title: {info.jobTitle}</p>
+          <p>Date of Work: {info.dateOfWork}</p>
+          <p>Responsibilites: {info.responsibilities}</p>
+          <button onClick = {handleEdit}> Edit </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Experience;
