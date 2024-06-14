f82d94677318f2de27d1e41da784af14b41ac5c9
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-14 12:57:23 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 import GeneralInfo from './generalInfo'
+import Education from './education'
 
 import '../styles/App.css'
 
 function App() {
+  //General Info
   const[generalInfo, setGeneralInfo] = useState({
     name: "",
     email: "",
     phone: ""
   })
-
   const handleGeneralInfo = (info) => {
     setGeneralInfo(info);
   }
-
+  //Experience
+  const[experience, setExperience] = useState({
+    title: "",
+    schoolName : "",
+    studyDate : ""
+  })
+  const handleExperienceChange = (info) => {
+    setExperience(info);
+  }
   return(
     <div className = 'app'>
       <h1>CV Builder</h1>
       <GeneralInfo generalInfo = {generalInfo} onGeneralInfoChange = {handleGeneralInfo}/>
+      <Education experience = {experience} onExperienceChange = {handleExperienceChange}/>
     </div>
   )
 }
