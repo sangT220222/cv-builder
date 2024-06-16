07f5e097e8206816c0b3158ee4ee1e632cab4565
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-16 10:20:38 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,59 @@
-import { useState } from 'react'
-import GeneralInfo from './generalInfo'
-import Education from './education'
-import Experience from './experience'
+import { useState } from "react";
+import GeneralInfo from "./generalInfo";
+import Education from "./education";
+import Experience from "./experience";
 
-import '../styles/App.css'
+import "../styles/App.css";
 
 function App() {
   //General Info
-  const[generalInfo, setGeneralInfo] = useState({
+  const [generalInfo, setGeneralInfo] = useState({
     name: "",
     email: "",
-    phone: ""
-  })
+    phone: "",
+  });
   const handleGeneralInfo = (info) => {
     setGeneralInfo(info);
-  }
+  };
 
   //Education
-  const[education, setEducation] = useState({
+  const [education, setEducation] = useState({
     title: "",
-    schoolName : "",
-    studyDate : ""
-  })
+    schoolName: "",
+    startDate: "",
+    endDate: "",
+  });
   const handleEducationChange = (info) => {
     setEducation(info);
-  }
+  };
 
   //Experience
-  const[experience, setExperience] = useState({
-    jobTitle: '',
-    dateOfWork: '',
-    responsibilities: ''
-  })
-  const handlexperienceChange = (info) =>{
+  const [experience, setExperience] = useState({
+    jobTitle: "",
+    startDate: "",
+    endDate: "",
+    responsibilities: "",
+  });
+  const handlexperienceChange = (info) => {
     setExperience(info);
-  }
-  return(
-    <div className = 'app'>
+  };
+  return (
+    <div className="app">
       <h1>CV Builder</h1>
-      <GeneralInfo generalInfo = {generalInfo} onGeneralInfoChange = {handleGeneralInfo}/>
-      <Education education = {education} onEducationChange = {handleEducationChange}/>
-      <Experience experience={experience} onExperienceChange={handlexperienceChange} />
+      <GeneralInfo
+        generalInfo={generalInfo}
+        onGeneralInfoChange={handleGeneralInfo}
+      />
+      <Education
+        education={education}
+        onEducationChange={handleEducationChange}
+      />
+      <Experience
+        experience={experience}
+        onExperienceChange={handlexperienceChange}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
