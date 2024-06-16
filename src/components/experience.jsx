4b6a85885f6e7073d333987b84ec03e3a5d7a89d
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-16 10:17:20 +0100

    Update experience.jsx

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,52 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-function Experience({experience, onExperienceChange}){
-  const[isEditing, setIsEditing] = useState(true);
-  const[info, setInfo] = useState(experience);
+function Experience({ experience, onExperienceChange }) {
+  const [isEditing, setIsEditing] = useState(true);
+  const [info, setInfo] = useState(experience);
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setInfo((prevInfo) => ({...prevInfo , [name] : value}));
-  }
+    const { name, value } = e.target;
+    setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
+  };
 
-  const handleSubmit = () =>{
-    setIsEditing(false);
-  } 
-
-  const handleEdit = () =>{
+  const handleSubmit = () => {
     onExperienceChange(info);
-    setIsEditing(true);
-  }
+    setIsEditing(false);
+  };
 
-  return(
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  return (
     <div className="experience">
       <h2>Experience</h2>
       {isEditing ? (
         <div>
           <label>
             Job Title
-            <input type = "text" name = "jobTitle" value = {info.jobTitle} onChange={handleChange}></input>
+            <input
+              type="text"
+              name="jobTitle"
+              value={info.jobTitle}
+              onChange={handleChange}
+            ></input>
           </label>
           <label>
             Date of Work
-            <input type = "text" name = "dateOfWork" value = {info.dateOfWork} onChange={handleChange}></input>
+            <input
+              type="date"
+              name="dateOfWork"
+              value={info.dateOfWork}
+              onChange={handleChange}
+            ></input>
           </label>
           <label>
             Responsibilities
-            <input type = "text" name = "responsibilities" value = {info.responsibilities} onChange={handleChange}></input>
+            <input
+              type="text"
+              name="responsibilities"
+              value={info.responsibilities}
+              onChange={handleChange}
+            ></input>
           </label>
-          <button onClick = {handleSubmit}>Submit</button>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
-      ):(
+      ) : (
         <div>
           <p>Job Title: {info.jobTitle}</p>
           <p>Date of Work: {info.dateOfWork}</p>
           <p>Responsibilites: {info.responsibilities}</p>
-          <button onClick = {handleEdit}> Edit </button>
+          <button onClick={handleEdit}> Edit </button>
         </div>
       )}
     </div>
-  )
+  );
 }
 
 export default Experience;
