76ac7857ba6340e195487f8791c1be55338ba434
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-16 17:20:34 +0100

    Update main.jsx\

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./components/App.jsx";  // Correct import path
-import './styles/index.css'; // Adjust path if necessary
+import "./styles/App.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
