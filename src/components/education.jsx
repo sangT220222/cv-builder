67e23ec3d14b06a06a98d476ad05879045fdc941
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-16 10:17:10 +0100

    Update education.jsx

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,48 +1,62 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-function Education({experience, onExperienceChange}){
+function Education({ education, onExperienceChange }) {
   const [isEditing, setIsEditing] = useState(true);
-  const [info, setInfo] = useState(experience);
-
+  const [info, setInfo] = useState(education);
   const handleChange = (e) => {
-    const {name,value} = e.target;
-    setInfo((prevInfo) => ({...prevInfo, [name] : value}));
-  }
+    const { name, value } = e.target;
+    setInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
+  };
 
   const handleSubmit = () => {
     onExperienceChange(info);
     setIsEditing(false);
-  }
+  };
 
   const handleEdit = () => {
     setIsEditing(true);
-  }
+  };
 
-  return(
+  return (
     <div className="experience">
       <h2>Experience</h2>
       {isEditing ? (
         <div>
           <label>
             Title
-            <input type = "text" name = "title" value = {info.title} onChange = {handleChange}></input>
+            <input
+              type="text"
+              name="title"
+              value={info.title}
+              onChange={handleChange}
+            ></input>
           </label>
           <label>
             School Name
-            <input type = "text" name = "schoolName" value = {info.schoolName} onChange = {handleChange}></input>
+            <input
+              type="text"
+              name="schoolName"
+              value={info.schoolName}
+              onChange={handleChange}
+            ></input>
           </label>
           <label>
             Study Dates
-            <input type = "text" name = "studyDate" value = {info.studyDate} onChange = {handleChange}></input>
+            <input
+              type="date"
+              name="studyDate"
+              value={info.studyDate}
+              onChange={handleChange}
+            ></input>
           </label>
-          <button onClick = {handleSubmit}> Submit </button>
+          <button onClick={handleSubmit}> Submit </button>
         </div>
-      ):(
+      ) : (
         <div>
           <p>Title: {info.title}</p>
           <p>School Name: {info.schoolName}</p>
           <p>Study dates: {info.studyDate}</p>
-          <button onClick = {handleEdit}> Edit </button>
+          <button onClick={handleEdit}> Edit </button>
         </div>
       )}
     </div>
